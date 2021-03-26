1babaed50e1bd0a331c48605c91d99d475eabfdc
Alejandro Pulido <alexpulido33@gmail.com>
2021-03-25 18:05:03 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
+import Translate from "./components/Translate";
+import Route from "./components/Route";
+import Header from "./components/Header";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const items = [
+	{
+		title: "what is React?",
+		content: "it is a front-end library",
+	},
+	{
+		title: "why use React?",
+		content: "because the immense popularit and range of implementing it",
+	},
+	{
+		title: "How do you use React?",
+		content: "with functional or class components and js",
+	},
+];
 
-export default App;
+const options = [
+	{
+		label: "the red",
+		value: "red",
+	},
+	{
+		label: "the green",
+		value: "green",
+	},
+	{
+		label: "the blue",
+		value: "blue",
+	},
+];
+
+export default () => {
+	const [selected, setSelected] = useState(options[0]);
+
+	return (
+		<div>
+			<Header />
+			<Route path={"/"}>
+				<Accordion items={items} />
+			</Route>
+
+			<Route path={"/list"}>
+				<Search />
+			</Route>
+
+			<Route path="/dropdown">
+				<Dropdown
+					label="select color"
+					options={options}
+					selected={selected}
+					onSelectedChange={setSelected}
+				/>
+			</Route>
+
+			<Route path={"/translate"}>
+				<Translate />
+			</Route>
+		</div>
+	);
+};
